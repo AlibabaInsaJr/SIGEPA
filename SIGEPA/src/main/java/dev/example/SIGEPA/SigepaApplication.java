4134bf1f765efff5281ff8b2bcba884480112edc
Alibaba <alibabajunyor@gamil.com>
2026-06-02 10:33:31 +0200

    Projeto inicial SIGEPA

---
+++ SIGEPA/src/main/java/dev/example/SIGEPA/SigepaApplication.java
@@ -0,0 +1,13 @@
+package dev.example.SIGEPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigepaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigepaApplication.class, args);
+	}
+
+}
---
+++ SIGEPA/src/test/java/dev/example/SIGEPA/SigepaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.example.SIGEPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigepaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
